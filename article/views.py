4fa984d8b93e597b5b7ed05f6feb3047381ae965
jieshenboy <917931079@qq.com>
2018-01-22 20:14:27 +0800

    add MyBlog

---
+++ article/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+from article.models import Article
+from django.http import Http404
+from datetime import datetime
+# Create your views here.
+def home(request):
+    post_list = Article.objects.all()
+    return render(request,'home.html', {'post_list': post_list})
+
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id = str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post': post})
