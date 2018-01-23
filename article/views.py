4ddfd167354ab5445fa67e529bee8faad9d2fbbc
jieshenboy <917931079@qq.com>
2018-01-23 12:24:38 +0800

    add Archives

--- article/views.py
+++ article/views.py
@@ -15,3 +15,17 @@ def detail(request, id):
     except Article.DoesNotExist:
         raise Http404
     return render(request, 'post.html', {'post': post})
+
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoseNotExist :
+        raise  Http404
+    return  render(request, 'archives.html', {'post_list' : post_list,
+                                              'error' : False})
+def search_tag(request, tag):
+    try:
+        post_list = Article.objects.filter(category = tag)
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'tag.html', {'post_list': post_list})
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^(?P<id>\d+)/$', views.detail, name = 'detail'),
+    url(r'^archives/$', views.archives, name='archives'),
+    url(r'^tag(?P<tag>\w+)/$',views.search_tag, name='search_tag'),
 ]
